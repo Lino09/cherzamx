bd8866d29d4e439f9891ef1ae40ba520dc077606
Abel <onosmx@gmail.com>
2018-09-05 14:38:42 -0500

    yaesta

---
+++ media/js/index.js
@@ -0,0 +1,17 @@
+var main = function() {
+
+
+  $('.selector').on("click", (event) => {
+    $(event.currentTarget).siblings().slideToggle();
+  });
+  $('.infobox').on("mouseleave", (event) => {
+    $('.selector').siblings().slideUp();
+  });
+
+
+
+
+
+
+};
+$(document).ready(main);
